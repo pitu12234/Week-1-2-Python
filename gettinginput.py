4b5491d801373ee8604e1a3e031abb213c21a1fd
pitu12234 <mudassarmaroof3@gmail.com>
2025-08-25 16:32:08 +0500

    Delete unnecessary files

--- gettinginput.py
+++
@@ -1,4 +0,0 @@
-
-name =input("enter the name :")
-age = input ("enter your age :")
-print ("hey "+name + "your age " + age )
--- workingwithstrings.py
+++
@@ -1,5 +0,0 @@
-
-name="mudassar"
-age= 22
-s=f"the name of the guy is {name} and age is {age},".capitalize().replace("guy","dick")
-print (s[s.index("g")].upper())
