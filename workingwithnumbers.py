0fd662fd53782798c41804521cf08da24ca94ee2
pitu12234 <mudassarmaroof3@gmail.com>
2025-08-25 12:13:36 +0500

    Update workingwithnumbers.py

--- workingwithnumbers.py
+++ workingwithnumbers.py
@@ -3,11 +3,13 @@ print("2") #strings
 num=2*7
 print(15%5+(num*num)) #precedence  
 sum=-5.11233
-print (abs(sum))
-print (pow(3,3))
-print (min(3,2))
-print (round(3.5))
-from math import *
-print (sqrt(196))
-print (floor(4.5))
-print (ceil(4.5))
+print (abs(sum)) #gives absolute value 5
+print (pow(3,3)) #power of numbers 
+print (min(3,2)) #gives the minimum value 
+print (round(3.5)) #round off number 
+from math import * #imports extra maths functions
+print (sqrt(196)) #gives the under root value
+print (floor(-5.1)) #round off towards downward 
+print (int(5.9)) #cuts off decimal
+print (ceil(9.1))
+print (round(4.3))
