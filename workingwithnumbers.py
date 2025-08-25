ad90961be3e8bf5eec81b6696bc87c75541b03f5
pitu12234 <mudassarmaroof3@gmail.com>
2025-08-25 11:57:29 +0500

    Create workingwithnumbers.py

---
+++ workingwithnumbers.py
@@ -0,0 +1,13 @@
+print(2)  #numbers to do maths with it 
+print("2") #strings 
+num=2*7
+print(15%5+(num*num)) #precedence  
+sum=-5.11233
+print (abs(sum))
+print (pow(3,3))
+print (min(3,2))
+print (round(3.5))
+from math import *
+print (sqrt(196))
+print (floor(4.5))
+print (ceil(4.5))
