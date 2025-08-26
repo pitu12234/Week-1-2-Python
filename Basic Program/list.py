024bd95cd37f296d2c3a5ed805873457c8f50ba7
pitu12234 <mudassarmaroof3@gmail.com>
2025-08-26 11:18:47 +0500

    Practice

--- Basic Program/list.py
+++ Basic Program/list.py
@@ -1,5 +1,10 @@
 friends= ["shazeb", "qasim","mudassar", "saad", "umair "]
 print(friends) #print list 
-friends[4]="jahangir"
+friends[4]="jahangir" #change in list 
 print(friends)
-print(friends[0:3])
+print(friends[0:3]) #range .
+
+'''''
+FOR LIST USE SQUARE BRACKET THAT CAN BE CHANGE LATER BUT ROIND 
+BRACKETS ARE FOR TUPPLE THAT CANT BE CHANGE
+'''
