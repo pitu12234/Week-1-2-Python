be7ff2ea017313a0ca313d2d7f9f62b4092db365
pitu12234 <mudassarmaroof3@gmail.com>
2025-08-26 11:12:15 +0500

    Update listfunctions.py

--- Basic Program/listfunctions.py
+++ Basic Program/listfunctions.py
@@ -1,15 +1,17 @@
 number= [12,34,1,67,9,8,12,44]
-friends= ["shazeb ", "qasim","mudassar", "saad", "umair"]
-#friends.extend(number)  #combine two  list
-#friends.append("tariq") #adding elements to the list 
-#friends.insert(1,"bus") #adding elements in the desire pos
-#friends.remove("shazeb ") #remove the specific element 
-#friends.pop() #remove the last element of the list by default 
-#friends.pop(1) #remove that element of the index 
-#friends.clear()  #clear the whole list 
-#number.sort() #sort list in ascending order
-#number.reverse() #reverse the list
-#print(number.index(12)) #to check if the element is in the list 
-#print (friends.count("umair")) #check the total number of same elements
-friends2=friends.copy() #copy the list 
-print(friends2)
+friends= ["shazeb", "qasim","mudassar", "saad", "umair"]
+'''''
+friends.extend(number)          #combine two  list
+friends.append("tariq")         #adding elements to the list 
+friends.insert(1,"bus")         #adding elements in the desire pos
+friends.remove("shazeb")       #remove the specific element 
+friends.pop()                   #remove the last element of the list by default 
+friends.pop(1)                  #remove that element of the index 
+friends.clear()                 #clear the whole list 
+number.sort()                   #sort list in ascending order
+number.reverse()                #reverse the list
+print(friends.index("shazeb"))   #to check if the element is in the list 
+print (friends.count("umair"))  #check the total number of same elements
+friends2=friends.copy()          #copy the list 
+print(friends)
+'''''
